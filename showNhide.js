d94bdb367e604623c175c3b296ffd992c844a92b
tlrebel <robetric@isu.edu>
2017-11-01 14:47:17 -0400

    added new files and made more changes

---
+++ showNhide.js
@@ -0,0 +1,30 @@
+
+  //  https://calendar.google.com/calendar/embed?src=o9qb4u58aa0vdfsba6hbmu50co%40group.calendar.google.com&ctz=America/New_York
+  
+//}
+function calender(container, load){
+    document.getElementById(container).innerHTML= load;
+}
+
+function duties(container, load) {
+    document.getElementById(container).innerHTML= load;
+   
+}
+
+function orderList(container, load) {
+    document.getElementById(container).innerHTML= load;
+    
+}
+
+function info(container, load) {
+    document.getElementById(container).innerHTML= load;
+}
+
+function hunt() {
+    window.alert("this works");
+}
+
+
+//duties.html
+//service.html
+//parentInfo.html
