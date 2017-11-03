a5478d86da21b30f3970d8bc6cb53c55edf2f8d7
tlrebel <robetric@isu.edu>
2017-11-03 17:16:07 -0400

    deleted some files, created two new files, and added drop down menu.

--- showNhide.js
+++ showNhide.js
@@ -2,14 +2,38 @@ function calender(container, load){
     document.getElementById(container).innerHTML= load;
 }
 
-function duties(container, load) {
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropDown(){
+ document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.second')) {
+
+    var dropdowns = document.getElementsByClassName("dropdownContent");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+function morning(container, load) {
     document.getElementById(container).innerHTML= load;
-   
+}
+
+function evening(container, load) {
+    document.getElementById(container).innerHTML= load;
 }
 
 function orderList(container, load) {
-    document.getElementById(container).innerHTML= load;
-    
+    document.getElementById(container).innerHTML= load; 
 }
 
 function info(container, load) {
