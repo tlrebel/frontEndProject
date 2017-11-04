3ae7b3c3d11da1d4d5b03ad6066df53704f65375
tlrebel <robetric@isu.edu>
2017-11-04 09:01:31 -0400

    fixed the drop down menu on both smaller and bigger screens

--- showNhide.js
+++ showNhide.js
@@ -4,14 +4,17 @@ function calender(container, load){
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function dropDown(){
- document.getElementById("myDropdown").classList.toggle("show");
+function big(){
+    document.getElementById("bigDropDown").classList.toggle("show");
+}
+
+function small(){
+    document.getElementById("smallDropDown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.second')) {
-
     var dropdowns = document.getElementsByClassName("dropdownContent");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
