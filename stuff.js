99fd7565bf6ad61db688d924e94860258f91cb4f
tlrebel <robetric@isu.edu>
2017-11-07 15:46:00 -0500

    created new css file for login button, styled eveingshifts and added more javascript to js file. Still working on parent info page.

--- stuff.js
+++ stuff.js
@@ -64,3 +64,13 @@ function save(){
     alert("This functionality not yet implemented");
 }
 
+// for log in button
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
