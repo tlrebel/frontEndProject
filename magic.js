3f21f3ec8782bcaf93e689206f258111e1e3356f
tlrebel <robetric@isu.edu>
2017-11-07 22:19:34 -0500

    Deleted files, cleaned up the codings, and renamed the file from stuff.js to magic.js.

---
+++ magic.js
@@ -0,0 +1,25 @@
+/* I created this file to help load the links selected links
+   to be displayed in desired content area.
+   created by: Tricia L. Roberts
+   Year: 2017
+   Code of Louisville Project
+ */
+
+// To load html file into content area
+function calender(container, load) {
+    document.getElementById(container).innerHTML = load;
+}
+
+function orderList(container, load) {
+    document.getElementById(container).innerHTML= load; 
+}
+
+// print the page
+function printThis() {
+    window.print();
+}
+
+// for the buttons that is not implemented
+function notReady() {
+    alert("This functionality not yet implemented");
+}
